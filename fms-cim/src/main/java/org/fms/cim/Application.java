277d94a1a9e180e23bc7ba34f2f8753eb747b04c
czy <czy@DESKTOP-9LNOG4H>
2020-10-22 16:11:15 +0800

    1

--- fms-cim/src/main/java/org/fms/cim/Application.java
+++ fms-cim/src/main/java/org/fms/cim/Application.java
@@ -6,8 +6,15 @@
 **/
 package org.fms.cim;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication(scanBasePackages = "org.fms.cim", exclude = MongoAutoConfiguration.class)
 public class Application {
 	public static void main(String[] args) {
-
+		SpringApplication.run(Application.class, args);
 	}
 }
