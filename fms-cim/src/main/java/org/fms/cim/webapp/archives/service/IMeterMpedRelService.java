7d3a576d34d868430cf0d76486eea5f2bfbd7449
qttcsm <2871375057@qq.com>
2020-10-28 15:54:19 +0800

    修改类

---
+++ fms-cim/src/main/java/org/fms/cim/webapp/archives/service/IMeterMpedRelService.java
@@ -0,0 +1,14 @@
+/**
+ *    Auth:riozenc
+ *    Date:2019年3月8日 下午3:38:03
+ *    Title:com.riozenc.cim.webapp.service.ICustomerService.java
+ **/
+package org.fms.cim.webapp.archives.service;
+
+import org.fms.cim.webapp.archives.domain.MeterMpedRelDomain;
+
+import com.riozenc.titanTool.spring.webapp.service.BaseService;
+
+public interface IMeterMpedRelService extends BaseService<MeterMpedRelDomain> {
+
+}
